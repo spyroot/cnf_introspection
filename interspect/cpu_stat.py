323f6a0d4ad4f9b84eec9c21250f2f0a098250f0
spyroot <spyroot@gmail.com>
2023-01-18 12:10:05 +0400

    refactored cmd

--- interspect/cpu_stat.py
+++ interspect/cpu_stat.py
@@ -60,23 +60,22 @@ def cpu_capability_stats():
     data_dict = {}
     try:
         if os.path.isfile("/proc/cpuinfo") and os.access("/proc/cpuinfo", os.R_OK):
-            proc_mem_fd = open("/proc/cpuinfo", 'r')
-            cpu_id = None
-            for line in proc_mem_fd:
-                data = line.split(":")
-                if data is not None and len(data) == 2:
-                    data_key = data[0].strip()
-                    data_value = data[1].strip()
-                    if 'processor' in data_key and len(data_key) > 0:
-                        cpu_id = data_value
-                        data_dict[cpu_id] = {}
-                        data_dict[cpu_id]['processor'] = data_value
-                    else:
-                        if 'flags' == data_key or 'vmx flags' == data_key or 'bugs' == data_key:
-                            data_dict[cpu_id][data_key] = dict.fromkeys(data_value.split(), True)
+            with open("/proc/cpuinfo", 'r', encoding="utf8") as proc_mem_fd:
+                cpu_id = None
+                for line in proc_mem_fd:
+                    data = line.split(":")
+                    if data is not None and len(data) == 2:
+                        data_key = data[0].strip()
+                        data_value = data[1].strip()
+                        if 'processor' in data_key and len(data_key) > 0:
+                            cpu_id = data_value
+                            data_dict[cpu_id] = {}
+                            data_dict[cpu_id]['processor'] = data_value
                         else:
-                            data_dict[cpu_id][data_key] = data_value
-            proc_mem_fd.close()
+                            if 'flags' == data_key or 'vmx flags' == data_key or 'bugs' == data_key:
+                                data_dict[cpu_id][data_key] = dict.fromkeys(data_value.split(), True)
+                            else:
+                                data_dict[cpu_id][data_key] = data_value
         return data_dict
     except FileNotFoundError as fnfe:
         print("Failed to access /proc/cpuinfo. Error", fnfe)
