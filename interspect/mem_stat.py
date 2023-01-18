ad8471ac59ddbae6ac6d30be9122d6d9eef4d808
spyroot <spyroot@gmail.com>
2023-01-18 07:48:45 +0400

    bash script

--- interspect/mem_stat.py
+++ interspect/mem_stat.py
@@ -10,8 +10,12 @@ def mem_info(cmd):
         if os.path.isfile("/proc/meminfo") and os.access("/proc/meminfo", os.R_OK):
             proc_mem_fd = open("/proc/meminfo", 'r')
             for line in proc_mem_fd:
-                data = line.split(":")[1:][0].strip()
-                print(data)
+                data = line.split(":")
+                if len(data) > 0:
+                    data_dict[data[0].strip()] = data[1].strio()
             proc_mem_fd.close()
     except FileNotFoundError as fnfe:
         print("You need to install lshw and ethtool first.")
+
+    print(data_dict)
+    return data_dict
