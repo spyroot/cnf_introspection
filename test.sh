bfb4df0fe259edb4a258fd901e88486da36f62ae
spyroot <spyroot@gmail.com>
2023-01-18 13:34:09 +0400

    vm stats

--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
 curl http://localhost:8000/adapters -H "Accept: application/json"
+
+
+python main.py cpu
+python main.py network
+python main.py numa
+python main.py kernel
+python main.py cpu_interrupt
+python main.py cpu_capability
+python main.py large_huge
+python main.py vmstat
+
